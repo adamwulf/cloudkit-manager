19351895b28ba6753c180ac87072a54874ae7a78
Bob Spryn <bob@lohilabs.com>
2014-06-11 21:11:49 -0600

    Initial Commit

---
+++ CloudKit Manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudKit Manager
+//
+//  Created by Bob Spryn on 6/11/14.
+//  Copyright (c) 2014 Sprynthesis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
